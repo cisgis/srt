9c246bcd5a4d38797587d7ac9dcb9febd3a4374e
cisgis <drizzle_lu@yahoo.com>
2026-04-25 15:46:27 -0500

    Logo file fix

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).parent
 DATA_DIR = BASE_DIR / "data"
 UPLOAD_DIR = DATA_DIR / "uploads"
 DB_PATH = DATA_DIR / "srt.db"
-LOGO_PATH = BASE_DIR / "examples" / "logo.webp"
+LOGO_PATH = BASE_DIR / "app" / "static" / "logo.webp"
 
 # Attachment subfolders
 PO_ATTACHMENTS_DIR = UPLOAD_DIR / "po_attachments"
